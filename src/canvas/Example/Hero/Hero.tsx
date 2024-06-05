54d4bdfac33d302b1d3cc4a936fea28a0dab6e0a
Robert van den Boorn <robertb@uniform.dev>
2024-06-05 19:24:37 +0200

    feat: gave hero component props mapped to Uniform component parameters

--- src/canvas/Example/Hero/Hero.tsx
+++ src/canvas/Example/Hero/Hero.tsx
@@ -1,18 +1,13 @@
 import Link from 'next/link';
 
-export const Hero = () => {
+export const Hero = ({ title, description }: { title: string; description: string }) => {
   return (
     <section className="flex items-center justify-center w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="container mx-auto px-4 md:px-8">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-              Example Component in Uniform
-            </h1>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-              Let&apos;s build our first Uniform component in React using Next.js and the Component Starter Kit as our
-              starting project.
-            </p>
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">{title}</h1>
+            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">{description}</p>
           </div>
           <div className="space-x-4">
             <Link
