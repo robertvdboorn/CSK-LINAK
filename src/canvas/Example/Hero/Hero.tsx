1d79394b4ca1e51e2e6be25f84fb444dcdbb3320
Robert van den Boorn <robertb@uniform.dev>
2024-06-05 19:26:25 +0200

    feat: changed hero component to support in-line editing

--- src/canvas/Example/Hero/Hero.tsx
+++ src/canvas/Example/Hero/Hero.tsx
@@ -1,13 +1,18 @@
+import { UniformText } from '@uniformdev/canvas-react';
 import Link from 'next/link';
 
-export const Hero = ({ title, description }: { title: string; description: string }) => {
+export const Hero = ({}: { title: string; description: string }) => {
   return (
     <section className="flex items-center justify-center w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="container mx-auto px-4 md:px-8">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">{title}</h1>
-            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">{description}</p>
+            <UniformText
+              as="h1"
+              parameterId="title"
+              className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none"
+            />
+            <UniformText as="p" parameterId="description" className="text-lg text-gray-500 dark:text-gray-400" />
           </div>
           <div className="space-x-4">
             <Link
